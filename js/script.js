af02e2cc52c5107a334e30078983ac96f7b42415
aartishchev <44969434+aartishchev@users.noreply.github.com>
2019-01-22 13:39:13 +0300

    Защита 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var bigbutton = document.querySelector (".main-button");
 var modalwindow = document.querySelector (".main-hotelsearch-form");
+var form = document.querySelector (".main-hotelsearch-form");
+var datetocome = document.querySelector ("[name=datearrival");
+var datetoleave = document.querySelector ("[name=datedeparture");
+var adults = document.querySelector ("[name=adultsnumber");
+
 
 if (bigbutton) {
     modalwindow.classList.add ("close-modalwindow")
@@ -8,5 +13,15 @@ if (bigbutton) {
 bigbutton.addEventListener ("click", function (evt) {
     evt.preventDefault();
     modalwindow.classList.toggle ("show-modalwindow");
-    modalwindow.classList.toggle ("close-modalwindow");
+    modalwindow.classList.toggle ("close-modalwindow")
+    form.classList.remove ("error-modalwindow");
 });
+
+form.addEventListener ("submit", function (evt) {
+    /* evt.preventDefault(); */
+    if (!datetocome.value || !datetoleave.value || !adults.value) {
+        evt.preventDefault();
+        form.classList.add ("error-modalwindow");
+      }
+});
+
